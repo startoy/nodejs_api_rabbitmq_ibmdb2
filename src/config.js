666ba973c14537df279aed10275366556c548787
LaziEgg <peerapat.kk@hotmail.com>
2018-09-17 15:02:57 +0700

    work but still no response to client(screen)

--- src/config.js
+++ src/config.js
@@ -11,5 +11,5 @@ export const hostname = process.env.HOSTNAME || '0.0.0.0';
 
 export const log_filename = process.env.LOG_NAME || 'access.log';
 export const amqp_uri = process.env.AMQP_URI || 'amqp://localhost';
-export const reply_to = process.env.REPLY_TO || 'amq.rabbit.reply-to';
+export const reply_to = process.env.REPLY_TO || 'rabbit.reply-to';
 export const rpc_queue = process.env.RPC_QUEUE || 'amq.direct';
--- src/rabbit.js
+++ src/rabbit.js
@@ -1,4 +1,5 @@
 import amqp from 'amqplib/callback_api'
+import "@babel/polyfill";
 
 import { randomid1 } from './util'
 import * as cnf from './config'
@@ -13,57 +14,63 @@ export async function initClient() {
 }
 
 export async function chClient(msg_send) {
-
 var msg_reply;
-await amqp.connect('amqp://localhost', function(err, conn) {
-  conn.createChannel(function(err, ch) {
-    ch.assertQueue('', {exclusive: true}, function(err, q) {
-      var corr = randomid1().toString();
+  try {
+    amqp.connect('amqp://localhost', (err, conn) => {
+      let corr = randomid1().toString();
+      let ch =  conn.createChannel()
+      ch.assertQueue(cnf.reply_to); 
 
-      console.log(' [x] Requesting fib(%d)', 10);
+      console.log(' [x] Request Okury')
 
-      ch.consume(q.queue, (msg)=>{
+  // consume
+       ch.consume(cnf.reply_to, msg => {
         if (msg.properties.correlationId == corr) {
-          console.log(' [.] Got %s', msg.content.toString());
-	  msg_reply = msg.content.toString();
+          console.log(' [.] Receive [%s]', msg.content.toString())
+          ch.ack(msg)
+          msg_reply = msg.content;
         }
-      }, {noAck: true});
+      }, {noAck:false})
 
-	if(msg_reply !== '')
-	 { return "555555555" }
 
-      ch.sendToQueue('rpc_queue',
-      new Buffer('10'),
-      { correlationId: corr, replyTo: q.queue });
+  // publish
+       ch.sendToQueue(/* cnf.rpc_queue */ 'rpc_queue',
+      new Buffer.from(/* msg_send */'10'),
+      { correlationId: corr, replyTo: cnf.reply_to})
+    })
+    return msg_reply;
+  } catch {
+    console.error('Error on chClient()')
+  }
+  return msg_reply;
+}
+
+/* 
+working example
+export async function chClient(msg_send) {
+
+  var msg_reply;
+  await amqp.connect('amqp://localhost', function(err, conn) {
+    conn.createChannel(function(err, ch) {
+      ch.assertQueue('', {exclusive: true}, function(err, q) {
+        var corr = randomid1().toString();
+  
+        console.log(' [x] Requesting fib(%d)', 10);
+  
+        ch.consume(q.queue, (msg)=>{
+          if (msg.properties.correlationId == corr) {
+            console.log(' [.] Got %s', msg.content.toString());
+      msg_reply = msg.content.toString();
+          }
+        }, {noAck: true});
+  
+    if(msg_reply !== '')
+     { return "555555555" }
+  
+        ch.sendToQueue('rpc_queue',
+        new Buffer('10'),
+        { correlationId: corr, replyTo: q.queue });
+      });
     });
   });
-});
-
-/*
-  //try {
-    let conn = await amqp.connect('amqp://localhost')
-    let corr = randomid1()
-    let ch = await conn.createChannel()
-    let q = await ch.assertQueue(cnf.reply_to); //self
-
-    console.log(' [x] Request Okury')
-    console.log(q.queue)
-
-// consume
-    await ch.consume(q.queue, msg => {
-      if (msg.properties.correlationId == corr) {
-        console.log(' [.] Receive [%s]', msg.conten.toString())
-        ch.ack(msg)
-
-      }
-    }, {noAck:false})
-
-// publish
-    ch.sendToQueue(cnf.rpc_queue,
-    new Buffer.from(msg_send.toString()),
-    { correlationId: corr, replyTo: q.queue})
- // } catch {
-  //  console.error('Error on chClient()')
- // }
-*/
-}
+} */
