38477a6bd1ac14e53a76436705fc02a1437dd61a
LaziEgg <peerapat.kk@hotmail.com>
2018-09-25 17:40:23 +0700

    reuse amqp but temp queue still remain

--- src/amqp.js
+++ src/amqp.js
@@ -1,94 +1,72 @@
 'use strict'
 
-/**
- * For now, can't use `async/await` way
- * use Promise (.then) instead
- * TODO: make use of `async/await`
- */
-
 import amqp from 'amqplib'
 import EventEmitter from 'events'
 
-import * as cnf from './config'
-import * as util from './util'
+async function connect(setting) {
+  const conn = await amqp.connect(setting.uri);
+  return conn;
+}
 
-// const REPLY_TO = cnf.reply_to;
-var THIS_QUEUE;
+async function channel(conn) {
+  const channel = await conn.createChannel();
+  channel.responseEmitter = new EventEmitter();
+  channel.responseEmitter.setMaxListeners(0);
+  return channel
+}
 
-/**
- * Create amqp Channel and return back the promise
- * 
- * @param {Object} params
- * @returns {Promise} - return amqp channel 
- */
-const createClient = (setting) => amqp.connect(setting.uri)
-  .then(conn => conn.createChannel()) // create channel
-  .then(channel => {
-    channel.responseEmitter = new EventEmitter();
-    channel.responseEmitter.setMaxListeners(0);
-    channel.assertQueue('', {
-        exclusive: true
-      })
-      .then(q_name => {
-        THIS_QUEUE = q_name.queue
-        console.log('\n ==== GENERATE PRIVATE QUEUE ====')
-        for (let prop in q_name) {
-          console.log(' =  Key[.%s][%s]', prop, q_name[prop])
-        }
-        console.log(' ================================ \n')
-      })
-
-    // emit (event, [arg1], [arg2], [...]) 
-    // Make an event listener for an event called "msg.properties.correlationId", then provoke the event
-    channel.consume(
-      THIS_QUEUE,
-      msg => channel.responseEmitter.emit(msg.properties.correlationId, msg.content), {
-        noAck: true
-      }
-    );
-    return channel;
+async function genQueue(channel){
+  const q = await channel.assertQueue('', {
+    exclusive: true
   });
+  return q;
+}
 
-/** 
- * return Promise obj when event emit from consume function
- * 
- * @param {Object} channel - amqp channel
- * @param {String} message - message to send to consumer (which is Okury)
- * @param {String} rpcQueue - name of the queue where will be sent to
- * @returns {Promise} - return msg that send back from Okury
- */
-const sendRPCMessage = (channel, message, rpcQueue) => new Promise(resolve => {
-  // unique random string
-  const correlationId = util.generateUuid();
+async function sendRPCMessage(channel, message, rpcQueue, q) {
+  return new Promise(resolve => {
+    // unique random string
+    const correlationId = generateUuid().toString();
+    channel.responseEmitter.once(correlationId, resolve);
+    channel.sendToQueue(rpcQueue, new Buffer.from(message), {
+      correlationId,
+      replyTo: q.queue
+    });
+  })
+}
 
-  // once (event, listener) Register a single listener for the specified event
-  // Invoked only once
-  channel.responseEmitter.once(correlationId, resolve);
+async function consumeRPCMessage(channel, q) {
+  try {
+    channel.consume(q.queue, msg => {
+      channel.responseEmitter.emit(msg.properties.correlationId, msg.content)
+      channel.close();
+    }, {
+      noAck: true
+    })
+  } catch (e) {
+    console.error('Cannot consume message from queue', e)
+  }
+}
 
-  /* channel.assertQueue(rpcQueue, {durable: false}); */
-  channel.sendToQueue(rpcQueue, new Buffer.from(message), {
-    correlationId,
-    replyTo: THIS_QUEUE
-  });
-});
-
-/**
- * just send msg to queue and end process
- * 
- * @param {Object} channel 
- * @param {String} message 
- * @param {String} Queue 
- */
-const sendQueueMessage = (channel, message, Queue) => {
-  channel.assertQueue(Queue, {
+async function sendToQueue(channel, Queue, message) {
+  await channel.assertQueue(Queue, {
     durable: false,
     autoDelete: true
   });
   channel.sendToQueue(Queue, new Buffer.from(message));
-};
+}
+
+function generateUuid() {
+  return Math.random().toString() +
+    Math.random().toString() +
+    Math.random().toString();
+}
+
 
 module.exports = {
-  createClient: createClient,
+  connect: connect,
+  channel: channel,
+  genQueue: genQueue,
   sendRPCMessage: sendRPCMessage,
-  sendQueueMessage: sendQueueMessage,
+  consumeRPCMessage: consumeRPCMessage,
+  sendToQueue: sendToQueue
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -11,15 +11,16 @@ import '@babel/polyfill'
 import client from '../amqp'
 import * as cnf from '../config'
 import util from 'util'
+import * as utl from '../util'
 
 const router = express.Router()
 
-let channel;
-client.createClient({
+let conn;
+client.connect({
     uri: cnf.amqp_uri
   })
-  .then(ch => {
-    channel = ch;
+  .then(c => {
+    conn = c;
   })
 
 // Logging middleware
@@ -30,101 +31,27 @@ router.use((req, res, next) => {
   next()
 });
 
-router.get('/direct/:message', async (req, res) => {
-  const message = req.params.message;
-  const queue_name = cnf.rpc_queue
-
-  client.sendQueueMessage(channel, message, queue_name)
-  let msg_response = util.format('Send To Queue [%s] Message [%s]', queue_name, message)
-  res.json(msg_response);
-});
 
 router.get('/direct/:queue_name/:message', async (req, res) => {
   const message = req.params.message;
   const queue_name = req.params.queue_name;
-
-  client.sendQueueMessage(channel, message, queue_name)
-  let msg_response = util.format('Send To Queue [%s] Message [%s]', queue_name, message)
-  res.json(msg_response);
 });
 
-router.get('/rpc/:message', async (req, res) => {
-  const message = req.params.message;
-  const queue_name = cnf.rpc_queue
-
-  client.sendRPCMessage(channel, message, queue_name)
-    .then(msg => {
-      console.log("Promise Receive [%s]", msg)
-      const result = msg.toString();
-      let msg_response = util.format('Send To Queue [%s] Message [%s] Result [%s]', queue_name, message, result)
-      res.json(msg_response);
-    });
-});
 
 router.get('/rpc/:queue_name/:message', async (req, res) => {
   const message = req.params.message;
   const queue_name = req.params.queue_name
-  
-  client.sendRPCMessage(channel, message, queue_name)
-    .then(msg => {
-      console.log("Promise Receive [%s]", msg)
-      const result = msg.toString();
-      let msg_response = util.format('Send To Queue [%s] Message [%s] Result [%s]', queue_name, message, result)
-      res.json(msg_response);
-    });
-});
-
-/**
- * TEST API
- * localhost:8080/rpc/test_queue/15/200
- */
-
-router.get('/rpc/:queue_name/:message/:iterate', async (req, res) => {
-  const message = req.params.message;
-  const queue_name = req.params.queue_name;
-  const iterate = req.params.iterate;
-
-  var start = new Date().getTime();
-  var count = 0;
-  var completed = 0;
-  var i = 0;
-
-console.log('START TIME [%d]', start);
-  for(i=0; i<iterate; i++) {
-    client.sendRPCMessage(channel, message, queue_name)
-      .then(msg => {
-        count++;
-        completed++;
-        // let msg_response = util.format('q[%s] msg[%s] r[%s]', queue_name, message, count)
-        // console.log(msg_response);
-        onComplete();
-      });
-  }
-
-  function onComplete() {
-    if (completed < i)
-      return;
-    let stop = Date.now();
-    console.log("**************** Summary ****************")
-    console.log('START TIME [%d]', start);
-    console.log('STOP  TIME [%d]', stop)
-    console.log('DIFF  TIME [%d]millisec ~ [%d]sec', stop - start, (stop - start)/1000)
-    console.log('Request Count [%d]', count)
-    console.log("*****************************************")
-    let jsonStr = JSON.stringify({
-      start_time: start,
-      stop_time: stop,
-      diff_mlsec: stop - start,
-      diff_sec: (stop - start)/1000,
-      req_times: count
-    })
-    res.json(JSON.parse(jsonStr));
+  try {
+    const channel = await client.channel(conn);
+    const q = await client.genQueue(channel)
+    client.consumeRPCMessage(channel, q)
+    const msg = await client.sendRPCMessage(channel, message, queue_name, q);
+    
+    console.log(msg);
+    res.json(msg.toString())
+  } catch(e) {
+    console.error('Error on API', e)
   }
 });
-
-router.get('/about', (req, res) => {
-  res.send('About Page...')
-})
-
 
 export default router
