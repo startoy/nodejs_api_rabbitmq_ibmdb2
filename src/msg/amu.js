9a2b0e9b91d2cf727d01ca307cb62a8672107ce8
Peerapat Suksri <peerapat_suk@freewillsolutions.com>
2018-12-14 18:01:36 +0700

    Fix Performance using * 1 instead of Number()

--- src/msg/amu.js
+++ src/msg/amu.js
@@ -15,18 +15,16 @@ function processAMU(msgType, msgStr) {
   // data preparation
   let loopGap = 13;
   let loopCount =
-    Number.isInteger(Number(buffer[6])) && Number(buffer[6] >= 0)
-      ? Number(buffer[6])
-      : 0;
+    Number.isInteger(buffer[6] * 1) && buffer[6] * 1 >= 0 ? buffer[6] * 1 : 0;
   let jsonStr = {
     MsgType: String(buffer[0].slice(0, 3)),
     cust_no: String(buffer[0].slice(3, 12)),
-    page: Number(buffer[1]),
-    total_page: Number(buffer[2]),
-    total_mkt: Number(buffer[3]),
-    total_call: Number(buffer[4]),
-    total_force: Number(buffer[5]),
-    loop_count: Number(buffer[6]),
+    page: buffer[1] * 1,
+    total_page: buffer[2] * 1,
+    total_mkt: buffer[3] * 1,
+    total_call: buffer[4] * 1,
+    total_force: buffer[5] * 1,
+    loop_count: buffer[6] * 1,
     data: []
   };
 
@@ -40,14 +38,15 @@ function processAMU(msgType, msgStr) {
         grade: String(buffer[8 + i]),
         stock_type: String(buffer[9 + i]),
         mrg_code: buffer[10 + i].padStart(3, '0'),
-        call_lmv: Number(buffer[11 + i]),
-        call_smv: Number(buffer[12 + i]),
-        sell_lmv: Number(buffer[13 + i]),
-        sell_smv: Number(buffer[14 + i]),
-        actual_vol: Number(buffer[15 + i]),
-        lastsale: Number(buffer[16 + i]),
-        call_margin: Number(buffer[17 + i]),
-        call_force: Number(buffer[18 + i]),
+        /* call_lmv: Number(buffer[11 + i]).toFixed(2), */
+        call_lmv: buffer[11 + i] * 1,
+        call_smv: buffer[12 + i] * 1,
+        sell_lmv: buffer[13 + i] * 1,
+        sell_smv: buffer[14 + i] * 1,
+        actual_vol: buffer[15 + i] * 1,
+        lastsale: buffer[16 + i] * 1,
+        call_margin: buffer[17 + i] * 1,
+        call_force: buffer[18 + i] * 1,
         ending_flag: String(buffer[19 + i])
       };
       jsonObj.data.push(msgObj);
