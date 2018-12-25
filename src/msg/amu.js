276da08709883973e84f55156eecec868ae62e22
Peerapat Suksri <peerapat_suk@freewillsolutions.com>
2018-12-25 10:41:01 +0700

    update msg AMU add field traderid tradername custname

--- src/msg/amu.js
+++ src/msg/amu.js
@@ -13,42 +13,48 @@ function processAMU(msgType, msgStr) {
   if (!msgStr) return 0;
   let buffer = msgStr.split(',');
 
-  // data preparation
-  let loopGap = 14;
-  let loopCount =
-    Number.isInteger(buffer[6] * 1) && buffer[6] * 1 >= 0 ? buffer[6] * 1 : 0;
   let jsonStr = {
     MsgType: String(msgType),
-    cust_no: String(buffer[0].slice(3, 12)),
-    page: String(buffer[1]),
-    total_page: String(buffer[2]),
-    total_mkt: String(buffer[3]),
-    total_call: String(buffer[4]),
-    total_force: String(buffer[5]),
-    loop_count: String(buffer[6]),
+    TraderID: String(buffer[1]),
+    TraderName: String(buffer[2]),
+    cust_no: String(buffer[3]),
+    cust_name: String(buffer[4]),
+    page: String(buffer[5]),
+    total_page: String(buffer[6]),
+    total_mkt: String(buffer[7]),
+    total_call: String(buffer[8]),
+    total_force: String(buffer[9]),
+    loop_count: String(buffer[10]),
     data: []
   };
+
+  // field loop_count
+  let loopCount =
+    Number.isInteger(buffer[10] * 1) && buffer[10] * 1 >= 0
+      ? buffer[10] * 1
+      : 0;
+  let loopGap = 14;
 
   let jsonObj = jsonStr;
   let loopMultiplyGap = loopCount * loopGap;
   if (loopCount >= 0) {
     for (let i = 0; i < loopMultiplyGap; i += loopGap) {
-      // start from 7, gap jump every 13
+      // start from 11, gap jump every 14
       let msgObj = {
-        stock_symbol: String(buffer[7 + i]),
-        grade: String(buffer[8 + i]),
-        stock_type: String(buffer[9 + i]),
-        mrg_code: String(buffer[10 + i]),
-        call_lmv: String(buffer[11 + i]),
-        call_smv: String(buffer[12 + i]),
-        sell_lmv: String(buffer[13 + i]),
-        sell_smv: String(buffer[14 + i]),
-        actual_vol: String(buffer[15 + i]),
-        lastsale: String(buffer[16 + i]),
-        mkt_value: String(buffer[17 + i]),
-        call_margin: String(buffer[18 + i]),
-        call_force: String(buffer[19 + i]),
-        ending_flag: String(buffer[20 + i])
+        stock_symbol: String(buffer[11 + i]),
+        grade: String(buffer[12 + i]),
+        stock_type: String(buffer[13 + i]),
+        mrg_code: String(buffer[14 + i]),
+        call_lmv: String(buffer[15 + i]),
+        call_smv: String(buffer[16 + i]),
+        sell_lmv: String(buffer[17 + i]),
+        sell_smv: String(buffer[18 + i]),
+        actual_vol: String(buffer[19 + i]),
+        lastsale: String(buffer[20 + i]),
+        mkt_value: String(buffer[21 + i]),
+        call_margin: String(buffer[22 + i]),
+        call_force: String(buffer[23 + i]),
+        ending_flag: String(buffer[24 + i])
       };
       jsonObj.data.push(msgObj);
     }
