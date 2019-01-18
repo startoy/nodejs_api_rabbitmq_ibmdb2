df48418e02973c8aaa7cf2226ec743196114f6be
Peerapat Suksri <peerapat_suk@freewillsolutions.com>
2019-01-18 14:17:48 +0700

    delete shell, add Dockerfile delete folder command

--- shell/cleardockerlog.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-API_CONTAINER_NAME='fwg/nodejs-api';
-
-id=$(docker ps | grep $API_CONTAINER_NAME | awk '{print $1}')
-
-echo 'Get PID '$id
-echo 'truncate -s 0 /var/lib/docker/containers/'$id'/'$id'-json.log'
-truncate -s 0 /var/lib/docker/containers/$id*/$id*-json.log
